46ef5d17ead35afa8d598f0082a07788149b0e7f
ShubhmAsati <38488068+ShubhmAsati@users.noreply.github.com>
2019-07-01 17:05:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express')
+
 var jwtAuth = require('./jwtAuth')
 
 const port = 8080
+
 var app = express();
+
 app.use(express.json());
+
+
+
+/* curl command curl -X POST   http://localhost:8080/signup   -H 'Content-Type: application/json'    -d '{
+    "userName": "shubham",
+    "userPassword": "1234567890"
+}' */
+/* output := {"token":"jwt_token"} */
+
 app.post('/signup',(req,res,next)=>{
 	let payLoad = {  // payload for jwt token , we will get same userName and password after decoding token .
 		userName: req.body.userName,
@@ -14,9 +26,13 @@ app.post('/signup',(req,res,next)=>{
 });
 
 
+//pass jwt token recieved from above request in header as {"authorization" :"Bearer jwt_token"}
 app.get('/login',jwtAuth.verifyToken,(req,res,next)=>{
 	res.status(200).json({"msg":"login Success"});
 });
+
+
+
 app.listen(port , ()=>{
 	console.log("--------------------------------- server started at port "+port+" -------------------------------");
 });
